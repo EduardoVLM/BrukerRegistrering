d757ff9e3bb5f0bf338dce1d19dae1400cddd69f
Eduardo Machado <144334218+EduardoVLM@users.noreply.github.com>
2024-04-24 14:46:36 +0200

    kobling til database

--- register.php
+++ register.php
@@ -1,22 +1,25 @@
 <?php
 // Database connection
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "your_database_name";
+$username = "root";
+$password = "Admin";
+$dbname = "brukerRegistrering";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
 // Get form data
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
 $email = $_POST['email'];
 
 // Insert data into database
-$sql = "INSERT INTO users (first_name, last_name, email) VALUES ('$fname', '$lname', '$email')";
+$sql = "INSERT INTO users (first_name, last_name, email) VALUES ('"$first_name'", '"$last_name"', '"$email"')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
